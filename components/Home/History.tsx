151b43ac0b3c59a68b2a0c177037ebf52d6acb8e
Jonathan <71105258+JAruliah@users.noreply.github.com>
2022-10-02 14:07:41 -0600

    scoreboard changes

--- components/Home/History.tsx
+++ components/Home/History.tsx
@@ -1,9 +1,11 @@
+import React, { useState } from 'react';
 import CardContent from '@mui/material/CardContent';
 import Card from '@mui/material/Card';
-import React from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import { Matches } from '@prisma/client';
+import Pagination from '@mui/material/Pagination';
+import Box from '@mui/material/Box';
 interface HistoryProps {
   matchHistory: Matches[];
 }
@@ -24,40 +26,44 @@ const months = [
 ];
 
 export const History = ({ matchHistory }: HistoryProps) => {
+  const [matchHistoryDupe, setMatchHistoryDupe] = useState(matchHistory);
+  const [skip, setSkip] = useState(0);
   return (
-    <Grid item xs={12} mb={4} sx={{ overflowY: 'scroll', height: '300px' }}>
-      {matchHistory
-        ?.slice(0)
-        .reverse()
-        .map((match) => {
-          let date = new Date(match.createdAt);
-          return (
-            <Card sx={{ width: '100%', marginBottom: '5px' }} key={match.id}>
-              <CardContent>
-                <Grid container justifyContent={'space-between'}>
-                  <Grid item>
-                    <Typography>
-                      Played:{' '}
-                      {`${
-                        months[date.getMonth()]
-                      }/${date.getDate()}/${date.getFullYear()}
+    <>
+      <Grid item xs={12} mb={4} sx={{ overflowY: 'scroll', height: '300px' }}>
+        {matchHistoryDupe
+          ?.slice(0)
+          .reverse()
+          .map((match) => {
+            let date = new Date(match.createdAt);
+            return (
+              <Card sx={{ width: '100%', marginBottom: '5px' }} key={match.id}>
+                <CardContent>
+                  <Grid container justifyContent={'space-between'}>
+                    <Grid item>
+                      <Typography>
+                        Played:{' '}
+                        {`${
+                          months[date.getMonth()]
+                        }/${date.getDate()}/${date.getFullYear()}
                       `}
-                    </Typography>
+                      </Typography>
+                    </Grid>
+                    <Grid item>
+                      <Typography>WPM: {match.WPM}</Typography>
+                    </Grid>
+                    <Grid item>
+                      <Typography>Accuracy: {match.accuracy}</Typography>
+                    </Grid>
+                    <Grid item>
+                      <Typography>Mode: {match.mode}</Typography>
+                    </Grid>
                   </Grid>
-                  <Grid item>
-                    <Typography>WPM: {match.WPM}</Typography>
-                  </Grid>
-                  <Grid item>
-                    <Typography>Accuracy: {match.accuracy}</Typography>
-                  </Grid>
-                  <Grid item>
-                    <Typography>Mode: {match.mode}</Typography>
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
-          );
-        })}
-    </Grid>
+                </CardContent>
+              </Card>
+            );
+          })}
+      </Grid>
+    </>
   );
 };
--- pages/scoreboard.tsx
+++ pages/scoreboard.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import axios from 'axios';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 
 interface scoreboardProps {
   scoreboard: {
@@ -20,6 +21,25 @@ interface scoreboardProps {
 }
 
 const scoreboard = (props: scoreboardProps) => {
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.secondary.main,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
   return (
     <Box>
       <Typography variant='h4' mb={2}>
@@ -29,25 +49,35 @@ const scoreboard = (props: scoreboardProps) => {
         <Table sx={{ minWidth: 650 }} aria-label='simple table'>
           <TableHead>
             <TableRow>
-              <TableCell>Username</TableCell>
-              <TableCell align='right'>WPM</TableCell>
-              <TableCell align='right'>Accuracy</TableCell>
-              <TableCell align='right'>Total Matches</TableCell>
+              <StyledTableCell>#</StyledTableCell>
+              <StyledTableCell>Username</StyledTableCell>
+              <StyledTableCell align='right'>WPM</StyledTableCell>
+              <StyledTableCell align='right'>Accuracy</StyledTableCell>
+              <StyledTableCell align='right'>Total Matches</StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {props.scoreboard.map((row) => (
-              <TableRow
+            {props.scoreboard.map((row, i) => (
+              <StyledTableRow
                 key={row.username}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
-                <TableCell component='th' scope='row'>
+                <StyledTableCell component='th' scope='row'>
+                  {i + 1}
+                </StyledTableCell>
+                <StyledTableCell component='th' scope='row'>
                   {row.username}
-                </TableCell>
-                <TableCell align='right'>{Math.round(row.wpm)}</TableCell>
-                <TableCell align='right'>{Math.round(row.accuracy)}</TableCell>
-                <TableCell align='right'>{row.totalMatches}</TableCell>
-              </TableRow>
+                </StyledTableCell>
+                <StyledTableCell align='right'>
+                  {Math.round(row.wpm)}
+                </StyledTableCell>
+                <StyledTableCell align='right'>
+                  {Math.round(row.accuracy)}
+                </StyledTableCell>
+                <StyledTableCell align='right'>
+                  {row.totalMatches}
+                </StyledTableCell>
+              </StyledTableRow>
             ))}
           </TableBody>
         </Table>
